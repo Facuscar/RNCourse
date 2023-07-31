406ad09b88dc20bce9f952f62ec0764544c7cd4f
Facundo <facuscardillo@gmail.com>
2023-07-30 21:52:19 -0300

    Styles to the form

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your goals!' />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your goals!' />
         <Button title='Add goal' />
       </View>
       <View>
@@ -20,5 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   }
 });
